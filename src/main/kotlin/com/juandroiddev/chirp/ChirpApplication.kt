57e1baa1411fb23985b43e25132885e76f1562da
juandroiddev <juandavidgaines@gmail.com>
2025-09-16 16:41:14 -0500

    initialize Chirp backend project with Gradle and Spring Boot setup

---
+++ src/main/kotlin/com/juandroiddev/chirp/ChirpApplication.kt
@@ -0,0 +1,11 @@
+package com.juandroiddev.chirp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChirpApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChirpApplication>(*args)
+}
