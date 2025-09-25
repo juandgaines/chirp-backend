ca203d1e0be58e0de50c0084ad3e32a1044d7594
juandroiddev <juandavidgaines@gmail.com>
2025-09-25 07:52:09 -0500

    feat: add RefreshTokenEntity and RefreshTokenRepository for managing refresh tokens

---
+++ user/src/main/kotlin/com/juandroiddev/chirp/infra/database/repositories/RefreshTokenRepository.kt
@@ -0,0 +1,11 @@
+package com.juandroiddev.chirp.infra.database.repositories
+
+import com.juandroiddev.chirp.domain.model.UserId
+import com.juandroiddev.chirp.infra.database.entities.RefreshTokenEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface RefreshTokenRepository: JpaRepository<RefreshTokenEntity,Long> {
+    fun findByUserIdAndHashedToken(userId: UserId, hashedToken: String): RefreshTokenEntity?
+    fun deleteByUserIdAndHashedToken(userId: UserId, hashedToken: String)
+    fun deleteByUserId(userId: UserId)
+}
