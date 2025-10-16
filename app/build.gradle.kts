51b9a331d23e5b80dd68e7705ddfbca92945241c
juandroiddev <juandavidgaines@gmail.com>
2025-10-16 12:40:46 -0500

    feat: configure Redis data source and add Redis starter dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation(libs.kotlin.reflect)
 
     implementation(libs.spring.boot.starter.security)
+
+    implementation(libs.spring.boot.starter.data.redis)
     implementation(libs.spring.boot.starter.data.jpa)
     runtimeOnly(libs.postgresql)
 /*
