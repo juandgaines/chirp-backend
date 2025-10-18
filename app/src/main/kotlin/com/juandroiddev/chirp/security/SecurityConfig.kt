87d77e6394e17703c2488893876dcef55e08d75a
juandroiddev <juandavidgaines@gmail.com>
2025-10-18 11:17:15 -0500

    feat: enhance security configuration to include JWT authentication filter and secure change password endpoint

--- app/src/main/kotlin/com/juandroiddev/chirp/security/SecurityConfig.kt
+++ app/src/main/kotlin/com/juandroiddev/chirp/security/SecurityConfig.kt
@@ -1,7 +1,7 @@
 package com.juandroiddev.chirp.security
 
+import com.juandroiddev.chirp.api.config.JWTAuthFilter
 import jakarta.servlet.DispatcherType
-import org.springframework.cglib.proxy.Dispatcher
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
@@ -9,18 +9,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class SecurityConfig {
 
     @Bean
-    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain{
+    fun filterChain(httpSecurity: HttpSecurity, jwtAuthFilter: JWTAuthFilter): SecurityFilterChain{
         return httpSecurity
             .csrf { it.disable() }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authorizeHttpRequests { auth->
                 auth.requestMatchers("/api/auth/**")
                     .permitAll()
+                    .requestMatchers("/api/auth/change-password")
+                    .authenticated()
                     .dispatcherTypeMatchers(
                         DispatcherType.ERROR,
                         DispatcherType.FORWARD
@@ -29,6 +32,7 @@ class SecurityConfig {
                     .anyRequest()
                     .authenticated()
             }
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
             .exceptionHandling { configurer ->
                 configurer
                     .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
