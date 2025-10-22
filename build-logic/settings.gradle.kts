f0973c945f5121f300f25626a33c0b0a4d94d143
juandroiddev <juandavidgaines@gmail.com>
2025-10-22 18:27:32 -0500

    feat: add version catalog for dependency management in settings.gradle.kts

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -7,4 +7,11 @@ dependencyResolutionManagement {
         gradlePluginPortal()
         mavenCentral()
     }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+
 }
