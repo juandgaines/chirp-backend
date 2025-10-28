6ef0e03f0c1f37c03514f2c0367cffe4dea39dc2
juandgaines <juandavidgaines@gmail.com>
2025-10-28 18:00:38 -0500

    feat: add EmailTemplateService for processing Thymeleaf templates

---
+++ notification/src/main/kotlin/com/juandroiddev/chirp/service/EmailTemplateService.kt
@@ -0,0 +1,23 @@
+package com.juandroiddev.chirp.service
+
+import org.springframework.stereotype.Service
+import org.thymeleaf.TemplateEngine
+import org.thymeleaf.context.Context
+
+@Service
+class EmailTemplateService (
+    private val templateEngine: TemplateEngine
+){
+
+    fun processTemplate(
+        templateName: String,
+        variables:Map<String, Any> = emptyMap()
+    ):String{
+        val context = Context().apply {
+            variables.entries.forEach { (key, value) ->
+                setVariable(key, value)
+            }
+        }
+        return templateEngine.process(templateName, context)
+    }
+}
