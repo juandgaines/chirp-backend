3ce2b59a4f0036a878f6229a7dd6412903a88ca3
juandgaines <juandavidgaines@gmail.com>
2025-12-02 08:25:50 -0500

    feat: add RestController annotation to ChatMessageController for API endpoint exposure

--- chat/src/main/kotlin/com/juandroiddev/chirp/api/controllers/ChatMessageController.kt
+++ chat/src/main/kotlin/com/juandroiddev/chirp/api/controllers/ChatMessageController.kt
@@ -6,7 +6,9 @@ import com.juandroiddev.chirp.service.ChatMessageService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @RequestMapping("/api/messages")
 class ChatMessageController (
     private val chatMessageService: ChatMessageService
